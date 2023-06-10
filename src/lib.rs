a73e8e9f97fb87b97080bc9bf67467d21db7c8f7
Mathspy <mathspy257@gmail.com>
2023-06-10 17:04:05 -0400

    Bootstrap chumsky based parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,20 @@
-pub struct Parser;
+use chumsky::{error::Rich, Parser as _};
+
+pub struct Parser<'a, 'b>
+where
+    'a: 'b,
+{
+    inner: chumsky::Boxed<'a, 'b, &'a str, AST, chumsky::extra::Err<Rich<'a, char>>>,
+}
 
 #[derive(Debug, PartialEq)]
 pub struct AST;
 
-impl Parser {
+impl<'a, 'b> Parser<'a, 'b> {
     pub fn new() -> Self {
-        Parser
+        Parser {
+            inner: chumsky::primitive::todo().boxed(),
+        }
     }
 
     pub fn parse(&self, _file: &str) -> AST {
@@ -13,7 +22,7 @@ impl Parser {
     }
 }
 
-impl Default for Parser {
+impl<'a, 'b> Default for Parser<'a, 'b> {
     fn default() -> Self {
         Self::new()
     }
