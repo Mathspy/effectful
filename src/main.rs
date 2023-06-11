9298db39446243941fb484921b13da2bb1fde212
Mathspy <mathspy257@gmail.com>
2023-06-10 22:02:33 -0400

    Make binary output AST based on stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io::Read;
+
+use effectful::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer = Vec::new();
+    let stdin = std::io::stdin();
+    let mut handle = stdin.lock();
+    handle
+        .read_to_end(&mut buffer)
+        .expect("unable to read stdin");
+
+    let input = std::str::from_utf8(&buffer).expect("valid utf8 input");
+
+    let parser = Parser::new();
+    let ast = parser.parse(input);
+    dbg!(ast);
 }
