363f7ddc378b98011bbda4f4613f5d50d11ac931
Mathspy <mathspy257@gmail.com>
2023-06-13 21:04:28 -0400

    Hack together a generator
    This is intentionally full of hacks because I wanna get to the effect
    system before I spend too much time on the HTML generator

---
+++ crates/generator/src/lib.rs
@@ -0,0 +1,57 @@
+use parser::{Expr, FunctionCallExpr, ModuleItem, AST};
+
+pub struct Generator;
+
+impl Generator {
+    pub fn new() -> Self {
+        Self
+    }
+
+    pub fn generate(&self, ast: &AST) -> String {
+        // TODO: better error handling
+        let main = ast.module.get("main").expect("missing main function");
+
+        let ModuleItem::Function(main) = main;
+
+        // TODO: Handle main return type not being Html
+        // TODO: Handle main not having a last expression
+        let ret = main
+            .body
+            .last()
+            .expect("main must have a return value of Html");
+
+        self.expr_to_html(ret)
+    }
+
+    // TODO: This will eventually need to be rewriting in effectful itself and be completed
+    fn html_std(call: &FunctionCallExpr) -> (&'static str, &[Expr]) {
+        match &call.name[..] {
+            "Html" => ("html", &call.children),
+            "Body" => ("body", &call.children),
+            "Paragraph" => ("p", &call.args[..1]),
+            _ => todo!(),
+        }
+    }
+
+    #[allow(clippy::only_used_in_recursion)]
+    fn expr_to_html(&self, expr: &Expr) -> String {
+        match expr {
+            Expr::StringLiteral(string_literal) => string_literal.clone(),
+            Expr::FunctionCall(call) => {
+                let (tag, children) = Self::html_std(call);
+                let children = children
+                    .iter()
+                    .map(|expr| self.expr_to_html(expr))
+                    .collect::<String>();
+
+                format!("<{tag}>{children}</{tag}>")
+            }
+        }
+    }
+}
+
+impl Default for Generator {
+    fn default() -> Self {
+        Self::new()
+    }
+}
